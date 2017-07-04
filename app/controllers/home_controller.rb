f3040a2f7fabe86e19ff8db604f2ad635e62d3ca
justinkhor <khorchengee@gmail.com>
2017-07-04 11:50:00 +0800

    changed home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @listings = Listing.all.order("created_at DESC")
+    @listings = Listing.all.order("created_at DESC").paginate(:page => params[:page]).per_page(20)
   end
 end
